ed8e4c383f6eefd956907a7d93e6a98a38ed74cf
EC2 Default User <ec2-user@ip-172-31-19-197.ap-northeast-1.compute.internal>
2016-08-19 07:30:04 +0000

    http server query string

--- webserver.go
+++ webserver.go
@@ -7,7 +7,11 @@ import (
 )
 
 func userHandler(w http.ResponseWriter, r *http.Request) {
+    param1 := r.URL.Query().Get("name")
+    param2 := r.URL.Query().Get("phone")
     io.WriteString(w,"中国公民钟某在入境越南胡志明市时发现护照被污损。总领事馆收集证据后，于27日上午向胡志明市外办提出交涉，指出污损中国护照有损中国公民国格、人格，是无耻的懦夫行为。")
+    io.WriteString(w, param1)
+    io.WriteString(w, param2)
 }
 
 func main() {
